b201724a7dfc589dc6c4763241379b16e241be60
Adam Kariv <adam.kariv@gmail.com>
2017-07-21 09:06:55 +0300

    Enable format guessing

--- datapackage_pipelines_assembler/processors/load_modified_resources.py
+++ datapackage_pipelines_assembler/processors/load_modified_resources.py
@@ -13,6 +13,9 @@ def modify_datapackage(dp, parameters, stats):
             if parameters['tabular'] == ('schema' in descriptor):
                 assert 'path' in descriptor
                 descriptor['url'] = resource_mapping[name]
+                if descriptor['url'].startswith('http'):
+                    # We append the path so that format guessing works
+                    descriptor['url'] += '#{}'.format(descriptor['path'])
                 dp['resources'].append(descriptor)
     return dp
 
