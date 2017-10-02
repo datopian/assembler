3d309b7afa10ab95ee65e16f73ea7ab55c15e1e7
Irakli Mchedlishvili <zelima@users.noreply.github.com>
2017-10-02 21:43:28 +0400

    Revert "Split the non-tabular node to generate one item per resource"

--- datapackage_pipelines_assembler/nodes/basic_nodes.py
+++ datapackage_pipelines_assembler/nodes/basic_nodes.py
@@ -49,25 +49,10 @@ class NonTabularProcessingNode(BaseProcessingNode):
         super(NonTabularProcessingNode, self).__init__(available_artifacts, outputs)
 
     def get_artifacts(self):
-        for artifact in self.available_artifacts:
-            datahub_type = 'derived/non-tabular'
-            resource_name = artifact.resource_name + '_{}'.format('non-tabular')
-            output = ProcessingArtifact(
-                datahub_type, resource_name,
-                [], [artifact],
-                [('assembler.update_resource',
-                  {
-                      'name': artifact.resource_name,
-                      'update': {
-                          'name': resource_name,
-                          'datahub': {
-                            'type': datahub_type,
-                            'derivedFrom': [
-                                artifact.resource_name
-                            ]
-                          }
-                      }
-                  })],
-                False
-            )
-            yield output
+        output = ProcessingArtifact(
+            None, 'non-tabular',
+            [], self.available_artifacts,
+            [],
+            False
+        )
+        yield output
