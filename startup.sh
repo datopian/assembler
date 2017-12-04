f14287058a7f72fe28ae919a01efa91a341c131d
Adam Kariv <adam.kariv@gmail.com>
2017-12-04 15:57:04 +0200

    Adapt to dpp startup changes

--- startup.sh
+++ startup.sh
@@ -7,7 +7,7 @@ dpp init
 echo "Deleting `redis-cli -n 6 -h redis KEYS '*' | wc -l` keys"
 redis-cli -h redis -n 6 FLUSHDB
 echo "Remaining `redis-cli -n 6 -h redis KEYS '*' | wc -l` keys"
-python3 -m celery -b $DPP_CELERY_BROKER -A datapackage_pipelines.app -l INFO beat &
+SCHEDULER=1 python3 -m celery -b $DPP_CELERY_BROKER -A datapackage_pipelines.app -l INFO beat &
 python3 -m celery -n mgmt -b $DPP_CELERY_BROKER --concurrency=1 -A datapackage_pipelines.app -Q datapackage-pipelines-management -l INFO worker &
 python3 -m celery -n work -b $DPP_CELERY_BROKER --concurrency=4 -A datapackage_pipelines.app -Q datapackage-pipelines -l INFO worker &
 dpp serve
